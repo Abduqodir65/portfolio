62a218e5c3b1f3dfe38a624bd15bdc7eb7bfb054
Abduqodir65 <abduqodirgithub@gmail.com>
2024-08-29 21:31:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let banner = document.querySelector(".banner");
+let toggle = document.querySelector(".toggle");
+
+toggle.onclick = function(){
+    toggle.classList.toggle("active");
+    banner.classList.toggle("active");
+}
